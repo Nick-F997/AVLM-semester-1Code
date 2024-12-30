a37a6797d27f592f52f0dcfdee88523c29e2e8de
Nick Fairburn <110976174+Nick-F997@users.noreply.github.com>
2024-12-30 10:49:05 +0000

    Added new files for each different network

--- matlab-scripts/alexNetClassification.m
+++ matlab-scripts/alexNetClassification.m
@@ -9,7 +9,7 @@ close all;
 %% Variable for reading in Unix or Windows filepaths
 windowsOrUnix = filesep;
 
-PATH = '../imageData/arucoChallengingSubdivided';
+PATH = 'imageData/arucoChallengingSubdivided';
 
 %% Training/Validation Split function
 trainingValidationSplit = 0.7;
@@ -22,49 +22,19 @@ batchSize = 16;
 %% Number of output classes
 numOutputs = 100;
 
-% % % Root strings
-% % root_path = sprintf('C:\Users\g3-atkinson\Desktop%s', windowsOrUnix);
-% % basic_imgs = sprintf('arucoBasic - cut%s', windowsOrUnix);
-% % challenging_imgs = sprintf('arucoChallenging - cut%s',windowsOrUnix);
-% % 
-% % % Weights strings
 weights_path = sprintf("..%sweights%s", windowsOrUnix, windowsOrUnix);
-% % basic_weights = sprintf("%sbasic_weights.mat", weights_path);
+
 complex_weights = sprintf("%scomplex_weights.mat", weights_path);
-% % combined_weights = sprintf("%scombined_weights.mat", weights_path);
-% % 
-% % % Get proper paths to images
-% % basic_path = sprintf("%s%s", root_path, basic_imgs);
-% % challenging_path = sprintf("%s%s", root_path, challenging_imgs);
-% % 
-% % % Data store of basic images
-% % dataStoreBasic = imageDatastore(basic_path,...
-% %                     'IncludeSubfolders',true,...
-% %                     'LabelSource','foldernames', 'ReadFcn', ...
-% %                     @(f) repmat(imresize(imread(f),[227 227]),[1,1,3]));
-% % 
-% Data store of distorted and noisy images
+
 dataStoreChallenging = imageDatastore(PATH,...
                     'IncludeSubfolders',true,...
                     'LabelSource','foldernames', 'ReadFcn', ...
                     @(f) repmat(imresize(imread(f),[227 227]),[1,1,3]));
 
-% % %% Data store of combined images
-% % dataStoreCombined = imageDatastore([basic_path, challenging_path],...
-% %                     'IncludeSubfolders',true,...
-% %                     'LabelSource','foldernames', 'ReadFcn', ...
-% %                     @(f) repmat(imresize(imread(f),[227 227]),[1,1,3]));
 
-
-%% Get different data sets and split them 
-% % [training_data_basic, testing_data_basic] = splitEachLabel(dataStoreBasic, ...
-% %                                         trainingValidationSplit, 'randomized');
 
 [training_data, testing_data] = splitEachLabel(dataStoreChallenging, ...
                                         trainingValidationSplit, 'randomized');
-
-% % [training_data_combined, testing_data_combined] = splitEachLabel(dataStoreCombined, ...
-% %                                         trainingValidationSplit, 'randomized');
 
 %% Bring in Alex net for basic layer
 anet = alexnet;
